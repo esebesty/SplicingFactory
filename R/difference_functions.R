5e261a2794db103ccef495b229a1d2a5864155e7
gallardoalba <gallardoalbac@gmail.com>
2026-01-21 02:21:26 +0100

    Remove unnecesary function

--- R/difference_functions.R
+++ R/difference_functions.R
@@ -9,6 +9,41 @@
 #' @param method Method to use for calculating the average splicing diversity
 #'   value in a condition. Can be \code{'mean'} or \code{'median'}.
 #' @return A \code{data.frame} with mean or median value of splicing diversity
+#'   across sample categories, the difference between these values and the log2
+#'   fold change values.
+#' @details The function uses a matrix of splicing diversity values in order to
+#' calculate mean or median differences and log2 fold changes between two
+#' conditions.
+#' @import stats
+calculate_fc <- function(x, samples, control, method = "mean") {
+    if (method == "mean") {
+        value <- aggregate(t(x), by = list(samples), mean, na.rm = TRUE)
+    }
+
+    if (method == "median") {
+        value <- aggregate(t(x), by = list(samples), median, na.rm = TRUE)
+    }
+
+    sorted <- value[value$Group.1 != control, ]
+    sorted[2, ] <- value[value$Group.1 == control, ]
+    value <- t(sorted[, -1])
+    value[is.nan(value[, 1]), c(1)] <- NA
+    value[is.nan(value[, 2]), c(2)] <- NA
+
+    result <- data.frame(value, ifelse(as.matrix(is.na(value[, 1]) | is.na(value[, 2])), NA, value[, 1] - value[,
+        2]), ifelse(as.matrix(is.na(value[, 1]) | is.na(value[, 2])), NA, as.matrix(log(value[, 1] / value[, 2],
+        base = 2))))
+    colnames(result) <- c(paste0(sorted[1, 1], "_", method), paste0(sorted[2, 1], "_", method), paste0(method,
+        "_difference"), "log2_fold_change")
+    return(result)
+}
+
+#' Calculate p-values using Wilcoxon rank sum test.
+#'
+#' @param x A \code{matrix} with the splicing diversity values.
+#' @param samples Character vector with an equal length to the number of columns
+#' in the input dataset, specifying the category of each sample.
+#' @param pcorr P-value correction method applied to the results, as defined in
 #' the \code{p.adjust} function.
 #' @param paired If \code{TRUE}, the Wilcox-test will be paired, and therefore
 #' it will be a signed rank test instead of the rank sum test.
@@ -71,4 +106,3 @@ label_shuffling <- function(x, samples, control, method, randomizations = 100,
     adjusted_p_values <- p.adjust(raw_p_values, method = pcorr)
     return(cbind(raw_p_values, adjusted_p_values))
 }
-
